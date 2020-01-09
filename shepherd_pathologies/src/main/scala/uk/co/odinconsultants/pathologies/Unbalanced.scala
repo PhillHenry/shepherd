5a5d7055e0eb98f535a1597268cc7b277cc788b1
PhillHenry <phillhenry@gmail.com>
2020-01-09 13:08:43 +0000

    Refactoring.

--- shepherd_pathologies/src/main/scala/uk/co/odinconsultants/pathologies/Unbalanced.scala
+++ shepherd_pathologies/src/main/scala/uk/co/odinconsultants/pathologies/Unbalanced.scala
@@ -2,16 +2,23 @@ package uk.co.odinconsultants.pathologies
 
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
-object Unbalanced {
+case class Datum(id: Long, value: String)
 
-  val idField = "id"
-  case class Datum(id: Long, value: String)
+object Unbalanced extends Serializable {
+
+  val LARGE_CLASS = "A"
+  val SMALL_CLASS = "B"
+
+  val idField     = "id"
+  val valueField  = "value"
+
+  def classBySplit(limit: Long)(i: Long): String = if (i < limit) LARGE_CLASS else SMALL_CLASS
 
   def write(n: Long, session: SparkSession, filename: String, format: String, ratio: Float): DataFrame = {
     import session.implicits._
-    val limit = (n * ratio).toLong
-    def classification(i: Long): String = if (i < limit) "A" else "B"
-    val df = session.range(n).map(i => Datum(i, classification(i)))
+    val limit             = (n * ratio).toLong
+    val classificationFn  = classBySplit(limit) _
+    val df                = session.range(n).map(i => Datum(i, classificationFn(i)))
     df.write.format(format).save(filename)
     session.read.format(format).load(filename)
   }
--- spark_integration_tests/src/test/scala/uk/co/odinconsultants/shepherd/SparkSpec.scala
+++ spark_integration_tests/src/test/scala/uk/co/odinconsultants/shepherd/SparkSpec.scala
@@ -4,22 +4,29 @@ import org.scalatest.{Matchers, WordSpec}
 import uk.co.odinconsultants.htesting.hdfs.HdfsForTesting._
 import uk.co.odinconsultants.htesting.spark.SparkForTesting._
 import uk.co.odinconsultants.pathologies.Unbalanced._
+import org.apache.spark.sql.functions._
 
 class SparkSpec extends WordSpec with Matchers {
 
   import session.implicits._
 
-  "Spark" should {
+  "Imbalanced data" should {
 
     val baseFilename = hdfsUri + System.currentTimeMillis()
 
     val filenameA = s"${baseFilename}_A"
     val filenameB = s"${baseFilename}_B"
     val ratio     = 0.9f
-    val dfA = write(10L, session, filenameA, "parquet", ratio)
-    val dfB = write(100L, session, filenameB, "parquet", ratio)
+    val dfA       = write(10L,  session, filenameA, "parquet", ratio)
+    val dfB       = write(100L, session, filenameB, "parquet", ratio)
 
-    "be up and running" in {
+    "aggregatable" in {
+      val df          = dfA.groupBy(valueField).agg(count(col(valueField)))
+      val class2Count = df.collect().map(r => r.getString(0) -> r.getLong(1)).toMap
+      class2Count(LARGE_CLASS) shouldBe 9
+      class2Count(SMALL_CLASS) shouldBe 1
+    }
+    "joinable" in {
       dfA.join(dfB, dfA(idField) === dfB(idField)).count() shouldBe 10
     }
   }
