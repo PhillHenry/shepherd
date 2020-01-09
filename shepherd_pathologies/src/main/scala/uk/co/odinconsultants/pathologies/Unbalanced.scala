d99f0d71cbfeaa11efc03d4119eaa85d6c6826e8
PhillHenry <phillhenry@gmail.com>
2020-01-09 12:25:19 +0000

    Refactor. Reads data that it writes.

--- shepherd_pathologies/src/main/scala/uk/co/odinconsultants/pathologies/Unbalanced.scala
+++ shepherd_pathologies/src/main/scala/uk/co/odinconsultants/pathologies/Unbalanced.scala
@@ -1,15 +1,17 @@
 package uk.co.odinconsultants.pathologies
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 object Unbalanced {
 
+  val idField = "id"
   case class Datum(id: Long, value: String)
 
-  def write(n: Long, session: SparkSession, filename: String, format: String): Unit = {
+  def write(n: Long, session: SparkSession, filename: String, format: String): DataFrame = {
     import session.implicits._
     val df = session.range(n).map(i => Datum(i, i.toString))
     df.write.format(format).save(filename)
+    session.read.format(format).load(filename)
   }
 
 }
--- spark_integration_tests/src/test/scala/uk/co/odinconsultants/shepherd/SparkSpec.scala
+++ spark_integration_tests/src/test/scala/uk/co/odinconsultants/shepherd/SparkSpec.scala
@@ -3,8 +3,7 @@ package uk.co.odinconsultants.shepherd
 import org.scalatest.{Matchers, WordSpec}
 import uk.co.odinconsultants.htesting.hdfs.HdfsForTesting._
 import uk.co.odinconsultants.htesting.spark.SparkForTesting._
-import uk.co.odinconsultants.pathologies.Unbalanced
-import uk.co.odinconsultants.pathologies.Unbalanced.write
+import uk.co.odinconsultants.pathologies.Unbalanced._
 
 class SparkSpec extends WordSpec with Matchers {
 
@@ -16,14 +15,10 @@ class SparkSpec extends WordSpec with Matchers {
 
     val filenameA = s"${baseFilename}_A"
     val filenameB = s"${baseFilename}_B"
-    write(10L, session, filenameA, "parquet")
-    write(100L, session, filenameB, "parquet")
+    val dfA = write(10L, session, filenameA, "parquet")
+    val dfB = write(100L, session, filenameB, "parquet")
 
     "be up and running" in {
-      val idField = "id"
-      val dfA = session.read.parquet(filenameA)
-      val dfB = session.read.parquet(filenameB)
-
       dfA.join(dfB, dfA(idField) === dfB(idField)).count() shouldBe 10
     }
   }
