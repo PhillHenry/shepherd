a2e03dfd2344900f018da50992ccf384b5fe3b75
PhillHenry <phillhenry@gmail.com>
2020-01-15 08:57:15 +0000

    Removed the assumption that we're talking to Hadoop.

--- spark_integration_tests/src/test/scala/uk/co/odinconsultants/shepherd/diagnostics/partitions/FilesSpec.scala
+++ spark_integration_tests/src/test/scala/uk/co/odinconsultants/shepherd/diagnostics/partitions/FilesSpec.scala
@@ -2,7 +2,7 @@ package uk.co.odinconsultants.shepherd.diagnostics.partitions
 
 import org.scalatest.{Matchers, WordSpec}
 import uk.co.odinconsultants.pathologies.TwoUnbalancedClasses._
-import uk.co.odinconsultants.pathologies.Unbalanced.LARGE_CLASS
+import uk.co.odinconsultants.pathologies.Unbalanced._
 
 class FilesSpec extends WordSpec with Matchers {
 
@@ -12,9 +12,10 @@ class FilesSpec extends WordSpec with Matchers {
     "have stats associed with it" in {
       val fileStats = filesStatsOf(dfFromDisk)
       fileStats should not be empty
-      println(fileStats.mkString("\n"))
-
-      fileStats.values.map(_.minAsString()).min shouldBe LARGE_CLASS
+      withClue(fileStats.mkString("\n")) {
+        val valueStats = fileStats.filter { case ((_, column), _) => column == valueField }
+        valueStats.values.map(_.minAsString).min shouldBe LARGE_CLASS
+      }
     }
   }
 
--- spark_shepherd/src/main/scala/uk/co/odinconsultants/shepherd/diagnostics/partitions/Files.scala
+++ spark_shepherd/src/main/scala/uk/co/odinconsultants/shepherd/diagnostics/partitions/Files.scala
@@ -1,28 +1,32 @@
 package uk.co.odinconsultants.shepherd.diagnostics.partitions
 
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.Path
-import org.apache.parquet.column.statistics.Statistics
-import org.apache.parquet.format.converter.ParquetMetadataConverter
-import org.apache.parquet.hadoop.ParquetFileReader
-import org.apache.spark.sql.{DataFrame, Dataset, Encoder}
+import org.apache.spark.sql.{DataFrame, Row}
 
-import scala.collection.JavaConverters._
+case class Statistics(count: Long, mean: Option[Double], stddev: Option[Double], minAsString: String, maxAsString: String)
 
 object Files {
 
-  def filesStatsOf[U <: Comparable[U]](ds: DataFrame): Map[String, Statistics[T] forSome {type T <: Comparable[T]}] = {
-    val conf = new Configuration()
-    conf.setBoolean("parquet.strings.signed-min-max.enabled", true)
-    ds.inputFiles.flatMap { file =>
-      val reader = ParquetFileReader.readFooter(conf, new Path(file), ParquetMetadataConverter.NO_FILTER)
-      for {
-        block   <- reader.getBlocks.asScala
-        column  <- block.getColumns.asScala
-      } yield {
-        file -> column.getStatistics
-      }
+  def filesStatsOf[U <: Comparable[U]](ds: DataFrame): Map[(String, String), Statistics] = {
+    val session = ds.sparkSession
+    val columns = ds.columns
+    val fileCols2Stats = for {
+      file    <- ds.inputFiles
+      column  <- columns
+    } yield {
+      val subDF = session.read.load(file)
+
+      (file, column) -> toStats(subDF.describe(column).collect())
+    }
+    fileCols2Stats.toMap
+  }
+
+  def toOptionalDouble(x: String): Option[Double] = Option(x).map(_.toDouble)
+
+  def toStats(rs: Array[Row]): Statistics = {
+    val name2Stat = rs.map { r =>
+      r.getString(0) -> r.getString(1)
     }.toMap
+    Statistics(name2Stat("count").toLong, toOptionalDouble(name2Stat("mean")), toOptionalDouble(name2Stat("stddev")), name2Stat("min"), name2Stat("max"))
   }
 
 }
